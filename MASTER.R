11006f4524863decd66e0a3c974e913e0e2eb283
Dasha Metropolitansky <metropolitansky@college.harvard.edu>
2019-05-03 17:41:04 -0400

    Uploaded files and created master

---
+++ MASTER.R
@@ -0,0 +1,8 @@
+
+library(tidyverse)
+library(readxl)
+
+parents = read_xlsx("parents.xlsx")
+students = read_xlsx("students.xlsx")
+teachers = read_xlsx("teachers.xlsx")
+quality_review = read_xlsx("quality_review.xlsx")
