93afd47129333a9cc268de36f9a2b9bff2557552
Dasha Metropolitansky <metropolitansky@college.harvard.edu>
2019-05-04 13:10:02 -0400

    uploaded new files

--- MASTER.R
+++ MASTER.R
@@ -6,7 +6,10 @@ parents = read_xlsx("parents.xlsx")
 students = read_xlsx("students.xlsx")
 teachers = read_xlsx("teachers.xlsx")
 quality_review = read_xlsx("quality_review.xlsx")
+<<<<<<< Updated upstream
 demo = read_xlsx("demographics.xlsx")
   
 
+=======
+>>>>>>> Stashed changes
 
