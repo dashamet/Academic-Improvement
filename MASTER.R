68cf2887bee4db4f11edaa4ad24e0145aad50296
Dasha Metropolitansky <metropolitansky@college.harvard.edu>
2019-05-04 13:31:08 -0400

    uploaded new files

--- MASTER.R
+++ MASTER.R
@@ -2,7 +2,7 @@
 library(tidyverse)
 library(readxl)
 
-quality <- read_xlsx("quality1.xlsx")
-survey <- read_xlsx("survey1.xlsx")
+quality <- read_xlsx("quality2.xlsx")
+survey <- read_xlsx("survey2.xlsx")
 demo <- read_xlsx("demographics1.xlsx")
 
