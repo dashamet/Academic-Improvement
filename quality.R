f195bbbc2c689291c9e68ee996713002d7414d83
Dasha Metropolitansky <metropolitansky@college.harvard.edu>
2019-05-04 13:52:34 -0400

    ran regression on quality_review

--- quality.R
+++ quality.R
@@ -7,7 +7,7 @@ library(tidyverse)
 library(boot)
 
 
-qual <- quality_review %>%
+qual <- quality %>%
   mutate(curriculum = as.numeric(quality_review_how_interesting_and_challenging_is_the_curriculum),
          effective = as.numeric(quality_review_how_effective_is_the_teaching_and_learning),
          assess = as.numeric(quality_review_how_well_does_the_school_assess_what_students_are_learning),
@@ -26,8 +26,7 @@ qual <- qual %>%
   mutate(curriculum = curriculum^0.5,
          assess = assess^0.5,
          expect = expect^-0.5,
-         teach = log(teach),
-         delta = delta^0.324)
+         teach = log(teach))
 
 fit_qual_delta <- with(qual, lm(reformulate(qual_vars, "delta")))
 summary(fit_qual_delta)
@@ -35,24 +34,25 @@ summary(fit_qual_delta)
 step_qual_delta_forward <- step(fit_qual_delta, direction = "forward")
 step_qual_delta_backward <- step(fit_qual_delta, direction = "backward")
 step_qual_delta_both <- step(fit_qual_delta, direction = "both")
-allpossregs(delta ~ curriculum + effective + assess + expect + teach, data = qual)
+#allpossregs(delta ~ curriculum + effective + assess + expect + teach, data = qual)
 
 summary(step_qual_delta_backward)
 summary(step_qual_delta_forward)
 summary(step_qual_delta_both)
 
-fit_qual_delta_2 <- lm(delta ~ factor(curriculum) + factor(effective) + factor(expect) + factor(teach), data = qual)
-
+#fit_qual_delta_2 <- lm(delta ~ factor(curriculum) + factor(effective) + factor(expect) + factor(teach), data = qual)
 
 model.glm1 = glm(formula(step_qual_delta_both), data = qual)
-model.glm2 = glm(formula(fit_qual_delta_2), data = qual)
+model.glm2 = glm(formula(step_qual_delta_forward), data = qual)
+#model.glm3 = glm(formula(fit_qual_delta_2), data = qual)
 
-cv.glm(data = qual, glmfit = model.glm1, K = 10)$delta[2]
-cv.glm(data = qual, glmfit = model.glm2, K = 10)$delta[2]
+cv.glm(data = qual, glmfit = model.glm1, K = 50)$delta[2]
+cv.glm(data = qual, glmfit = model.glm2, K = 50)$delta[2]
+#cv.glm(data = qual, glmfit = model.glm3, K = 10)$delta[2]
 
-vif(fit_qual_delta_2)
-resettest(fit_qual_delta_2)
-mmps(fit_qual_delta_2)
-ad.test(residuals(fit_qual_delta_2))
-ncvTest(fit_qual_delta_2)
-plot(fit_qual_delta_2)
+vif(step_qual_delta_forward)
+resettest(step_qual_delta_forward)
+mmps(step_qual_delta_forward)
+ad.test(residuals(step_qual_delta_forward))
+ncvTest(step_qual_delta_forward)
+plot(step_qual_delta_forward)
